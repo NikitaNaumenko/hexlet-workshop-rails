cba21971ab7c6e2e78929d5e6169257ea8da17be
Naumenko Nikita <naumenkoniki@gmail.com>
2018-02-03 00:27:59 +0300

    add specs

--- test/models/article/comment_test.rb
+++ test/models/article/comment_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class Article::CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save comment without article" do
+    comment = Article::Comment.new
+    assert_not comment.save, "Saved the article without a title"
+  end
+
+  test "should  save coment with comment and body" do
+    article = Article.create(title: '12354')
+    comment = Article::Comment.new(article: article, body: '12345', commentor: '123456')
+    assert comment.save, "Saved the comment with body and commentor"
+  end
+
+  test "should save comment with article" do
+    article = Article.create(title: '12345')
+    comment = Article::Comment.new(article: article)
+    assert comment.save, "Saved the comment with a article"
+  end
 end
--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save article without title" do
+    article = Article.new
+    assert_not article.save, "Saved the article without a title"
+  end
+
+  test "should not save article with short title" do
+    article = Article.new(title: '1234')
+    assert_not article.save, "Saved the article with short a title"
+  end
+
+  test "should save article with title" do
+    article = Article.new(title: '12345')
+    assert article.save, "Saved the article with a title"
+  end
 end
+
